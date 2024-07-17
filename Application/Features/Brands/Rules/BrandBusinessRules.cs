b00c8a2917b4c4120e7732dbca7d6a93ed0b0336
Kerem DURAK <kerem.durak@yahoo.com>
2024-07-17 17:25:05 +0300

    upp

--- Application/Features/Brands/Rules/BrandBusinessRules.cs
+++ Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -1,13 +1,18 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Brands.Constants;
+using Application.Services.Repositories;
+using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Application.Features.Brands.Rules;
 
-public class BrandBusinessRules
+public class BrandBusinessRules : BaseBusinessRules
 {
     private readonly IBrandRepository _brandRepository;
 
@@ -16,6 +21,13 @@ public class BrandBusinessRules
         _brandRepository = brandRepository;
     }
 
+    public async Task BrandNameCannotBeDuplicatedWhenInserted(string name)
+    {
+        Brand? result = await _brandRepository.GetAsync(predicate: b => b.Name.ToLower() == name.ToLower());
 
-
+        if (result!=null)
+        {
+            throw new BusinessException(BrandsMessages.BrandNameExists);
+        }
+    }
 }
