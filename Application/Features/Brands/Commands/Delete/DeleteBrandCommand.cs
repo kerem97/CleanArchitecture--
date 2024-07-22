7c225e204e08defdbb362b9b778f6635df7cfd9e
Kerem DURAK <kerem.durak@yahoo.com>
2024-07-22 17:35:37 +0300

    upp

--- Application/Features/Brands/Commands/Delete/DeleteBrandCommand.cs
+++ Application/Features/Brands/Commands/Delete/DeleteBrandCommand.cs
@@ -2,6 +2,7 @@
 using Application.Services.Repositories;
 using AutoMapper;
 using Core.Application.Pipelines.Caching;
+using Core.Application.Pipelines.Logging;
 using Domain.Entities;
 using MediatR;
 using System;
@@ -12,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Delete;
 
-public class DeleteBrandCommand : IRequest<DeletedBrandResponse>, ICacheRemoverRequest
+public class DeleteBrandCommand : IRequest<DeletedBrandResponse>, ICacheRemoverRequest, ILoggableRequest
 {
     public Guid Id { get; set; }
 
