24cd901774a070e465116a5ea2aac86cd623fc90
Kerem DURAK <kerem.durak@yahoo.com>
2024-07-26 16:13:05 +0300

    updated

---
+++ Persistence/EntityConfigurations/OperationClaimConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class OperationClaimConfiguration : IEntityTypeConfiguration<OperationClaim>
+{
+    public void Configure(EntityTypeBuilder<OperationClaim> builder)
+    {
+        builder.ToTable("OperationClaims").HasKey(oc => oc.Id);
+
+        builder.Property(oc => oc.Id).HasColumnName("Id").IsRequired();
+        builder.Property(oc => oc.Name).HasColumnName("Name").IsRequired();
+        builder.Property(oc => oc.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(oc => oc.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(oc => oc.DeletedDate).HasColumnName("DeletedDate");
+
+        builder.HasQueryFilter(oc => !oc.DeletedDate.HasValue);
+
+        builder.HasMany(oc => oc.UserOperationClaims);
+
+        //builder.HasData(_seeds);
+    }
+}
--- Persistence/PersistenceServiceRegistration.cs
+++ Persistence/PersistenceServiceRegistration.cs
@@ -19,10 +19,15 @@ public static class PersistenceServiceRegistration
         //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("narchitecture"));
         services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
         services.AddScoped<IBrandRepository, BrandRepository>();
-        services.AddScoped<IFuelRepository, FuelRepository>();
         services.AddScoped<IModelRepository, ModelRepository>();
-        services.AddScoped<ITransmissionRepository, TransmissionRepository>();
-        services.AddScoped<ICarRepository, CarRepository>();
+
+        services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
+        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        services.AddScoped<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
         return services;
     }
 }
